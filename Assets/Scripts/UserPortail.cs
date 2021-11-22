68a741f041cb0a56e9b36c1e7b156fa0d94d2ab9
Asurakasan <constantin.harchy@lacatholille.fr>
2021-11-22 15:47:37 +0100

    Portail

---
+++ Assets/Scripts/UserPortail.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserPortail : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if(currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Portail>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = null;
+        }
+    }
+}
